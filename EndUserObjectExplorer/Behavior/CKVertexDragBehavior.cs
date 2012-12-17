014cbd0bd0f3f9d105a701fa64b7c1fd90354f3f
Nementon <roose@intechinfo.fr>
2012-12-18 00:32:40 +0100

    Added drags behaviors

---
+++ EndUserObjectExplorer/Behavior/CKVertexDragBehavior.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Windows;
+using System.Windows.Interactivity;
+using EndObjectExplorer.Model;
+using EndUserObjectExplorer.Control;
+using GraphSharp.Controls;
+
+namespace EndObjectExplorer.Behavior
+{
+    public class CKVertexDragBehavior : Behavior<CKVertexControl>
+    {
+        private Point _mouseLastPositionOnCanvas;
+        private GraphCanvas _graphCanvas;
+        private bool _isCtrlDown = false;
+
+        protected override void OnAttached()
+        {
+            AssociatedObject.MouseLeftButtonDown += (sender, e) =>
+            {
+                _graphCanvas = AssociatedObject.RootCanvas;
+                _mouseLastPositionOnCanvas = e.GetPosition(_graphCanvas);
+                AssociatedObject.CaptureMouse();
+            };
+
+            AssociatedObject.MouseLeftButtonUp += (sender, e) =>
+            {
+                AssociatedObject.ReleaseMouseCapture();
+            };
+
+            AssociatedObject.MouseMove += (sender, e) =>
+            {
+                if (AssociatedObject.IsMouseCaptured && _isCtrlDown)
+                {
+                    Point mouseCurrentPositionOnCanvas = e.GetPosition(_graphCanvas);
+                    if (_mouseLastPositionOnCanvas != mouseCurrentPositionOnCanvas)
+                    {
+                        Vector diff = _mouseLastPositionOnCanvas - mouseCurrentPositionOnCanvas;
+                        
+                        GraphCanvas.SetX(AssociatedObject, GraphCanvas.GetX(AssociatedObject) - diff.X);
+                        GraphCanvas.SetY(AssociatedObject, GraphCanvas.GetY(AssociatedObject) - diff.Y);
+                        
+                        _mouseLastPositionOnCanvas = mouseCurrentPositionOnCanvas;
+                    }
+                }
+            };
+        }
+    }
+}
---
+++ EndUserObjectExplorer/Behavior/CKVertexSubGraphDragBehavior.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Windows;
+using System.Windows.Interactivity;
+using EndObjectExplorer.Model;
+using EndUserObjectExplorer.Control;
+using GraphSharp.Controls;
+
+namespace EndObjectExplorer.Behavior
+{
+    public class CKVertexSubGraphDragBehavior : Behavior<CKVertexControl>
+    {
+        private List<CKVertexControl> _linkedVertexControl = new List<CKVertexControl>();
+        private Point _mouseLastPositionOnCanvas;
+        GraphCanvas _graphCanvas;
+
+
+        protected override void OnAttached()
+        {
+            AssociatedObject.MouseLeftButtonDown += (sender, e) =>
+            {
+                _graphCanvas = AssociatedObject.RootCanvas;
+                _mouseLastPositionOnCanvas = e.GetPosition(_graphCanvas);
+                _linkedVertexControl = FindLinkedVertexControls();
+                AssociatedObject.CaptureMouse();
+            };
+
+            AssociatedObject.MouseLeftButtonUp += (sender, e) =>
+            {
+                AssociatedObject.ReleaseMouseCapture();
+            };
+
+            AssociatedObject.MouseMove += (sender, e) =>
+            {
+                if (AssociatedObject.IsMouseCaptured)
+                {
+                    Point mouseCurrentPositionOnCanvas = e.GetPosition(_graphCanvas);
+                    if (_mouseLastPositionOnCanvas != mouseCurrentPositionOnCanvas)
+                    {
+                        Vector diff = _mouseLastPositionOnCanvas - mouseCurrentPositionOnCanvas;
+                        foreach (var vc in _linkedVertexControl)
+                        {
+                            GraphCanvas.SetX(vc, GraphCanvas.GetX(vc) - diff.X);
+                            GraphCanvas.SetY(vc, GraphCanvas.GetY(vc) - diff.Y);
+                        }
+                        _mouseLastPositionOnCanvas = mouseCurrentPositionOnCanvas;
+                    }
+                }
+            };
+        }
+
+        List<CKVertexControl> FindLinkedVertexControls()
+        {
+            var vertex = AssociatedObject.Vertex as CKVertex;
+            Debug.Assert(vertex != null);
+
+            List<IVertex> linkedVertices = vertex.OwnerGraph.FindLinkedVertexOf(vertex);
+            List<CKVertexControl> linkedVerticesControl = new List<CKVertexControl>();
+            linkedVerticesControl.Add(AssociatedObject);
+
+            foreach (UIElement element in _graphCanvas.Children)
+            {
+                if (element is CKVertexControl)
+                {
+                    CKVertexControl ckV = (CKVertexControl)element;
+                    if (linkedVertices.Contains(ckV.Vertex) && !linkedVerticesControl.Contains(ckV))
+                    {
+                        linkedVerticesControl.Add(ckV);
+                    }
+                }
+            }
+            return linkedVerticesControl;
+        }
+    }
+}
