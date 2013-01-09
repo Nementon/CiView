17d92a61621ce69b39ff7180a471ed58e3351e7d
Paty bruno <paty@intechinfo.fr>
2013-01-09 17:27:17 +0100

    add model filter

---
+++ ActivityLogger/ActivityLogger.Tests/FilterTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ActivityLogger.SewerModel;
+using ActivityLogger.Filters;
+using ActivityLogger.Sink;
+using NUnit.Framework;
+using CK.Core;
+
+namespace ActivityLogger.Tests
+{
+    [TestFixture]
+    public class FilterTest
+    {
+        [Test]
+        public void DeleteBranches()
+        {
+            BagItems bag = new BagItems();
+            LineItem root = new LineItem("Root", LogLevel.Trace, bag);
+            LineItem rootChild = new LineItem("rootChild", LogLevel.Trace, bag);
+            LineItem rootChildChild = new LineItem("RootChildFirstChild", LogLevel.Error, bag);
+
+            bag.InsterRootItem(root);
+            bag.InsterRootItem(rootChild);
+            bag.InsterRootItem(rootChildChild);
+
+            ModelFilter mf = new ModelFilter(1, bag);
+            mf.DeleteBranches();
+            Console.WriteLine(bag.FirstChild.ToString());
+
+        }
+    }
+}
--- ActivityLogger/Filters/ModelFilter.cs
+++ ActivityLogger/Filters/ModelFilter.cs
@@ -2,10 +2,35 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using CK.Core;
+using ActivityLogger.SewerModel;
 
 namespace ActivityLogger.Filters
 {
-    class ModelFilter
+   public class ModelFilter
     {
+        #region Properties
+        public BagItems Host {  get;  set; }
+        public int NumChildren { get; set; }
+        #endregion
+      
+        public ModelFilter(int numChildren,BagItems host)
+        {
+            NumChildren = numChildren;
+            Host = host;
+        }
+        
+        public void DeleteBranches()
+        {
+            if(NumChildren<Host.ChildrenNumber)
+            {
+                int j = Host.ChildrenNumber - NumChildren;
+                for (int i=0;i<j;i++)
+                {
+                    Host.FirstChild.Delete();
+                }
+            }
+        }
+
     }
 }
